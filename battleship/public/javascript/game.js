6796f995684940ed2aed774ac8c01787fad00cd4
aaron.zhao <aaron.c.zhao@outlook.com>
2018-12-04 20:57:09 +0100

    click implemented

--- battleship/public/javascript/game.js
+++ battleship/public/javascript/game.js
@@ -1,4 +1,22 @@
+//entry of the program
+var main = function(){
+
 //create the datastructer for players
+
+
+
 $("td").click(function(){
-    console.log("click");
+    var grids = this;
+    console.log(grids.id);
 });
+
+
+
+
+
+
+
+
+
+}
+$(document).ready(main);
