8f08ca9412fa783decdd774889e3aa583db2e70a
aaron.zhao <aaron.c.zhao@outlook.com>
2018-12-06 20:47:12 +0100

    index completed

--- battleship/routes/index.js
+++ battleship/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/splash', function(req, res) {
+  res.render('splash.html', { root: './public' });
 });
+
+//get game page when hit the play button
+router.get('/play', function(req,res){
+  res.render('game.html', {root: './public'})
+})
 
 module.exports = router;
