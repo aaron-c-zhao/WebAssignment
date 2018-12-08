6b0d895fc4734bb1869e4aedfda0a62182e79248
aaron.zhao <aaron.c.zhao@outlook.com>
2018-12-08 21:22:21 +0100

    place reimplement complete

--- battleship/public/javascript/interactions.js
+++ battleship/public/javascript/interactions.js
@@ -1,83 +1,26 @@
 
-function Board(boardId){
-    
-    this.boardId = boardId;
-    
-    this.coordinatsMatrix = [
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-    ];
-    
-    //used to find an element in a multi dimansional array
-    // var id = function(){
+function setUp(){
 
-    // }
+    var ship = new Ship();
 
-    this.idParserInWard = function(id){
-        var x = id.substr(0,1);
-        var y = id.substr(1,1);
-        return [x,y];
-    }
+    $("#placeShip").click(function(){
+        
+        document.getElementById("gameboard_1").style.backgroundColor = "rgb(255,255,255,0.8)";
+        
+        //$(".shipTypes").show();
+        var type = [];
+        $(".shipTypes").click(function(){
+            var thisShip = this.value;
+            console.log(thisShip);
+            if(type.indexOf(thisShip) < 0){
+                type.push(thisShip);
+                    ship.placeship(thisShip);
+                }
+        })
+    });
 
-    this.idParserOutWard = function(id){
-        var gridId = id[0].toString() + id[1].toString();
-        return gridId;
-    }
-
-    //board = "gamebpard_player1 td"or player2
-    this.enableBoard = function(boardId){
-       var id;
-       $(boardId).click(function(){
-           id = this.id;
-       });
-       return idParserInWard(id);
-    }
-
-    //TODO:disable click on board
-    this.disableBoard = function(){
-
-    }
-
-    this.setElement = function(matrixId, value){
-        this.coordinatsMatrix[matrix[0]][matrixId[1]] = value;
-    }
-
-    //has this grid been occupied
-    this.isValidClick = function(id){
-        return this.coordinatsMatrix[id[0]][id[1]] === 0 || this.coordinatsMatrix[id[0]][id[1]] === 42;
-    }
-
-    this.highlight = function(gridId, color){
-        document.getElementById(gridId).style.backgroundColor = color;
-    }
-
-    this.deHighLight = function(matrixId){
-        document.getElementById(this.idParserOutWard(matrixId)).style.backgroundColor = "none";
-    }
-}
-
-
-var GameState = function(){
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return{
+  
 
-    }
-}();
+};
+$(document).ready(setUp);
