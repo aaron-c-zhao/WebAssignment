be10866660c4949dc9c70bb4252da575f715b174
aaron.zhao <aaron.c.zhao@outlook.com>
2018-12-08 22:47:48 +0100

    web socket

--- battleship/app.js
+++ battleship/app.js
@@ -1,48 +1,38 @@
-var createError = require('http-errors');
+
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var websocket = require("ws");
+var http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var port = process.argv[2];
+const filename = process.argv[2];
+var port = process.argv[3];
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var server = http.createServer(app);
+const wss = new websocket.Server({ server });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//TODO game object
+// var currentGame = null;
+// var connectionID = 0;
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+wss.on("connection", function connection(ws){
+  // let con = ws;
+  // con.id = connectionID++;
+  //TODO game object
+  //let playerID = currentGame.addPlayer(con);
+  ws.on("message", function incoming(message){
+    console.log(ws.readyState);
+  })
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-app.get('/', function(req, res){
-  res.send()
+
+
 })
 
 
+server.listen(port, function(){
+  console.log("listening on port" + port);
+});
 
--- battleship/public/javascript/interactions.js
+++ battleship/public/javascript/interactions.js
@@ -3,6 +3,7 @@ function setUp(){
 
     var ship = new Ship();
 
+    //place ship
     $("#placeShip").click(function(){
         
         document.getElementById("gameboard_1").style.backgroundColor = "rgb(255,255,255,0.8)";
@@ -11,13 +12,19 @@ function setUp(){
         var type = [];
         $(".shipTypes").click(function(){
             var thisShip = this.value;
-            console.log(thisShip);
+            //TODO:use disable to disable buttons
             if(type.indexOf(thisShip) < 0){
                 type.push(thisShip);
                     ship.placeship(thisShip);
                 }
         })
     });
+
+    
+
+
+
+
 
     
   
