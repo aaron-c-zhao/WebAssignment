5f93629cfec85f04fdc51ab7c3918f95e33ac99f
aaron.zhao <aaron.zhaocr@gmail.com>
2018-12-11 19:24:32 +0100

    sign off version

--- battleship/public/javascript/giveMeCookie.js
+++ battleship/public/javascript/giveMeCookie.js
@@ -1,17 +1,23 @@
-document.cookie = "vistTimes=0";
-
-var cookieArray = document.cookie.split('; ');
-var cookies={};
 
 
-for(var i = 0; i < cookieArray.length; i++){
-    var cookie = cookieArray[i].split("=")
-    cookies[cookie[0]]=cookie[1];
-}
+// var cookieArray = document.cookie.split('; ');
+// var cookies={};
+
+// if(!document.cookie){
+
+// }
+//     for(var i = 0; i < cookieArray.length; i++){
+//         var cookie = cookieArray[i].split("=")
+//         cookies[cookie[0]]=cookie[1];
+//     }
+
+// if(cookie["vistTimes"] == undefined)
+//     var visitTimes = 0;
 
 
-var visitTimes = parseInt(cookies["vistTimes"]);
-visitTimes++;
-var stringVisitTimes = visitTimes.toString();
 
-document.cookie = "vistTimes" + "=" + stringVisitTimes + ";";
+// var visitTimes = parseInt(cookies["vistTimes"]);
+// visitTimes++;
+// var stringVisitTimes = visitTimes.toString();
+
+// document.cookie = "vistTimes" + "=" + stringVisitTimes + ";";
